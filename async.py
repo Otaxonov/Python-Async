fab7a4dc7c39b89454e4263da76f7324438b7e36
Otaxonov <Otaxonov_98@mail.ru>
2025-03-27 14:22:41 +0500

    Python Async

--- async.py
+++ async.py
@@ -1,8 +1,22 @@
 import asyncio
+from time import perf_counter
+from asyncio import sleep
 
-async def task():
-    print("Hey...")
-    await asyncio.sleep(1)
-    print("I am here...")
+async def write():
+    print('Hey')
+    await sleep(1)
+    print('there')
 
-asyncio.run(task())
+async def main():
+    await asyncio.gather(
+        write(), write(), write(), write(), write()
+    )
+
+if __name__ == '__main__':
+
+    start = perf_counter()
+    asyncio.run(main())
+    end = perf_counter()
+
+    elapsed = end - start
+    print(f'Executed in {elapsed:.2f} seconds.')
--- sync.py
+++ sync.py
@@ -6,12 +6,14 @@ def write():
     print('there')
 
 def main():
-    for _ in range(10):
+    for _ in range(5):
         write()
 
 if __name__ == '__main__':
+
     start = perf_counter()
     main()
     end = perf_counter()
+
     elapsed = end - start
     print(f'Executed in {elapsed:.2f} seconds.')
